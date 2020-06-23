11f02c0a52c516de01ca1c2c6b341083275f0605
Gokula Priya <gokulapriya4@gmail.com>
2020-06-23 17:58:03 +0530

    adding problems

--- src/main/java/DataStructures/RepeatedString/RepeatedString.java
+++ src/main/java/DataStructures/RepeatedString/RepeatedString.java
@@ -1,4 +1,21 @@
 package main.java.DataStructures.RepeatedString;
 
 public class RepeatedString {
+
+    static long repeatedString(String s, long n) {
+        return (n / s.length() * getNoOfOccuranceofCharacter(s, 'a')) +
+                getNoOfOccuranceofCharacter(s.substring(0, (int) (n % s.length())), 'a');
+    }
+
+    public static long getNoOfOccuranceofCharacter(String s, char target) {
+        return s.codePoints().filter(ch -> ch == target).count();
+    }
+
+    public static void main(String[] args) {
+        String s = "afd";
+        long n = 1;
+
+        System.out.println(repeatedString(s, n));
+
+    }
 }
